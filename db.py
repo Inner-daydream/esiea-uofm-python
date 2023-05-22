ec8205631802f02509f78f6922c2ed75cec36eb3
Lucas <contact@lucasquitman.fr>
2023-05-22 15:46:27 -0400

    create db class for cur / conn state

--- db.py
+++ db.py
@@ -1,8 +1,16 @@
 import sqlite3 as sql
 
-def init(database_name):
-    conn = sql.connect(database_name)
-    cur = conn.cursor()
-    cur.execute('''CREATE TABLE IF NOT EXISTS submissions
-                 (id TEXT PRIMARY KEY, title TEXT, timestamp INTEGER, nsfw INTEGER)''')
-    conn.commit()
+class Database(object):
+    def __init__(self,database_name):
+        self.conn = sql.connect(database_name)
+        self.cur = self.conn.cursor()
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS submissions
+                    (id TEXT PRIMARY KEY, title TEXT, timestamp INTEGER, nsfw INTEGER)''')
+        self.conn.commit()
+
+    def insert_submission(self, id, title, timestamp, nsfw):
+        self.cur.execute('''INSERT INTO submissions VALUES (?, ?, ?, ?)''', (id, title, timestamp, nsfw))
+        self.conn.commit()
+
+    def close(self):
+        self.conn.close()
