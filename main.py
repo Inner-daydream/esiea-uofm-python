81f3cfb6fdcf9829a0f1e0423a35135ea0863693
Lucas <contact@lucasquitman.fr>
2023-05-22 15:45:39 -0400

    intercept sigint

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import reddit
-import db
+from db import Database
 import signal, sys
+from functools import partial
 # handle sigint
-def signal_handler(sig, frame):
+def signal_handler(database, sig, frame):
     # cleanup here
-    print('\nExiting...')
+    database.close()
     sys.exit(0)
-signal.signal(signal.SIGINT, signal_handler)
 
 def main():
     if __name__=='__main__':
-        # db.init('reddit.db')
-        reddit.query_popular_nsfw(10, 10)
+        # init database
+        database = Database('reddit.db')
+        # Register signal handler
+        signal.signal(signal.SIGINT, partial(signal_handler, database))
+
+        reddit.query_popular_nsfw(popular_limit=10, 
+                                  submission_limit=20, 
+                                  database=database)
 
 main()
