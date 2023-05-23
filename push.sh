05b25cb043247f52b6084d928a93d3f950b1d095
Lucas <contact@lucasquitman.fr>
2023-05-23 14:11:25 -0400

    fixed push script

--- push.sh
+++ push.sh
@@ -6,6 +6,6 @@ else
     echo "$CR_PAT" | docker login ghcr.io -u "$GH_USERNAME" --password-stdin
 fi
 lowercase_username=$(echo "$GH_USERNAME" | tr '[:upper:]' '[:lower:]')
-
+pip freeze > requirements.txt
 docker buildx build --platform linux/amd64 -t "ghcr.io/$lowercase_username/pyreddit:latest" .
 docker push "ghcr.io/$lowercase_username/pyreddit:latest"
