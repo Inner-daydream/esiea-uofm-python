8411865a9d1164202e8291eec2cad7676d9b1921
Lucas <contact@lucasquitman.fr>
2023-05-22 22:54:11 -0400

    docker integration

---
+++ push.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ "$PASSWORD_MANAGER" = "1password" ]; then
+    #shellcheck disable=SC2016
+    op run -- zsh -c 'echo $CR_PAT | docker login ghcr.io -u $GH_USERNAME --password-stdin'
+else
+    echo "$CR_PAT" | docker login ghcr.io -u "$GH_USERNAME" --password-stdin
+fi
+lowercase_username=$(echo "$GH_USERNAME" | tr '[:upper:]' '[:lower:]')
+
+docker buildx build --platform linux/amd64 -t "ghcr.io/$lowercase_username/pyreddit:latest" .
+docker push "ghcr.io/$lowercase_username/pyreddit:latest"
